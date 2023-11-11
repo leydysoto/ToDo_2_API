a9d96df155218c75a17bfd4a5152b8c9e6f564c7
leydy soto <leydysot.20.ja@gmail.com>
2023-11-11 00:58:17 -0500

    find all by status

--- src/main/java/com/leydy/todoapp/controller/TaskController.java
+++ src/main/java/com/leydy/todoapp/controller/TaskController.java
@@ -1,10 +1,14 @@
 package com.leydy.todoapp.controller;
 
 import com.leydy.todoapp.model.Task;
+import com.leydy.todoapp.model.TaskStatus;
 import com.leydy.todoapp.model.dto.TaskRequestDTO;
 import com.leydy.todoapp.service.TaskService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping("/tasks")
@@ -15,6 +19,15 @@ public class TaskController {
     @PostMapping
     public Task createTask(@RequestBody TaskRequestDTO taskRequestDTO){
         return taskService.createTask(taskRequestDTO);
+    }
+
+    @GetMapping
+    public List<Task> findAll(){
+        return taskService.findAll();
+    }
+    @GetMapping("/taskStatus/{status}")
+    public List<Task> findAllByStatus(@PathVariable("status") TaskStatus status){
+        return  taskService.findAllByTaskStatus(status);
     }
 
 
--- src/main/java/com/leydy/todoapp/service/TaskService.java
+++ src/main/java/com/leydy/todoapp/service/TaskService.java
@@ -2,10 +2,14 @@ package com.leydy.todoapp.service;
 
 import com.leydy.todoapp.mapper.TaskRequestDTOtoTask;
 import com.leydy.todoapp.model.Task;
+import com.leydy.todoapp.model.TaskStatus;
 import com.leydy.todoapp.model.dto.TaskRequestDTO;
 import com.leydy.todoapp.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @AllArgsConstructor
 @Service
 public class TaskService {
@@ -15,6 +19,14 @@ public class TaskService {
     public Task createTask(TaskRequestDTO taskRequestDTO){
         Task task=mapper.map(taskRequestDTO);
         return repository.save(task);
+    }
+    public List<Task> findAll(){
+        return repository.findAll();
+
+    }
+    public List<Task> findAllByTaskStatus(TaskStatus taskStatus){
+        return repository.findAllByTaskStatus(taskStatus);
+
     }
 
 
