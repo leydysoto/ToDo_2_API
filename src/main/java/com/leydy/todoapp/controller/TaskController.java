f3fa18f845c2f17b7b30473bdc177b6478a3dd55
leydy soto <leydysot.20.ja@gmail.com>
2023-11-11 01:44:35 -0500

    delete task

--- src/main/java/com/leydy/todoapp/controller/TaskController.java
+++ src/main/java/com/leydy/todoapp/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.leydy.todoapp.model.TaskStatus;
 import com.leydy.todoapp.model.dto.TaskRequestDTO;
 import com.leydy.todoapp.service.TaskService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +29,16 @@ public class TaskController {
     @GetMapping("/taskStatus/{status}")
     public List<Task> findAllByStatus(@PathVariable("status") TaskStatus status){
         return  taskService.findAllByTaskStatus(status);
+    }
+    @PatchMapping("/markFinished/{id}")
+    public ResponseEntity<Void>markAsFinished(@PathVariable("id") Long id){
+        taskService.taskAsFinished(id);
+        return  ResponseEntity.noContent().build();
+    }
+    @DeleteMapping("/deleteTask/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("id")Long id){
+        taskService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 
--- src/main/java/com/leydy/todoapp/service/TaskService.java
+++ src/main/java/com/leydy/todoapp/service/TaskService.java
@@ -1,14 +1,18 @@
 package com.leydy.todoapp.service;
 
+import com.leydy.todoapp.exception.ToDoExcepcions;
 import com.leydy.todoapp.mapper.TaskRequestDTOtoTask;
 import com.leydy.todoapp.model.Task;
 import com.leydy.todoapp.model.TaskStatus;
 import com.leydy.todoapp.model.dto.TaskRequestDTO;
 import com.leydy.todoapp.repository.TaskRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -27,6 +31,22 @@ public class TaskService {
     public List<Task> findAllByTaskStatus(TaskStatus taskStatus){
         return repository.findAllByTaskStatus(taskStatus);
 
+    }
+    @Transactional
+    public void taskAsFinished(Long id){
+        Optional<Task> toptionalTask =repository.findById(id);
+        if(toptionalTask.isEmpty()){
+            throw new ToDoExcepcions("task not found", HttpStatus.NOT_FOUND);
+
+        }
+        repository.markTaskAsFinished(id);
+    }
+    public void deleteById(Long id){
+        Optional<Task>optionalTask=repository.findById(id);
+        if(optionalTask.isEmpty()){
+            throw new ToDoExcepcions("task not found",HttpStatus.NOT_FOUND);
+        }
+        repository.deleteById(id);
     }
 
 
